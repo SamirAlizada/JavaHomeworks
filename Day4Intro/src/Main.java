684f47dc538cc70137cda5a3f5fe5d38f1157a11
furkanceri <furkancerii2@gmail.com>
2021-05-04 14:53:28 +0300

    Homework

--- Day4Intro/src/Main.java
+++ Day4Intro/src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 	public static void main(String[] args) throws RemoteException{
 		Customer customer= new Customer();
 		customer.setId(1);
-		customer.setNationalityId(1234567891);
+		customer.setNationalityId(12345678911L);
 		customer.setFirstName("Furkan");
 		customer.setLastName("Çeri");
 		customer.setBirthYear(1995);
